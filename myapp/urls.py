0e2ab051afeab59bf6ebd9b0f40c78a43717c40c
Fran Hrzenjak <fran@changeset.hr>
2014-03-22 18:50:36 +0100

    correct home url entry

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import patterns, url
 from .views import Home, List, Create, Update
 
 urlpatterns = patterns('',
+    url(r'^$', Home.as_view(), name='home'),
     url(r'^list/', List.as_view(), name='list'),
     url(r'^create/', Create.as_view(), name='create'),
     url(r'^update/(?P<pk>[\d-]+)/', Update.as_view(), name='update'),
-    url(r'', Home.as_view(), name='home'),
 )
